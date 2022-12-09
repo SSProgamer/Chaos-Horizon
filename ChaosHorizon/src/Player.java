1157bd9485d2fbab7e833ea75f6a53bfc155ba66
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2022-12-09 22:30:04 +0700

    ได้หน้าจอแล้ว เย่!!!

---
+++ ChaosHorizon/src/Player.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(x, y, 32, 32);
+    }
+}
