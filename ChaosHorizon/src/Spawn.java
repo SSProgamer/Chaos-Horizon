d28da1ee7010bd705a64452d77d36ad8f7896bc2
Thanawat sritho <64070044@kmitl.ac.th>
2022-12-13 21:31:46 +0700

    new Wave
    test wave.java

--- ChaosHorizon/src/Spawn.java
+++ ChaosHorizon/src/Spawn.java
@@ -11,10 +11,44 @@ public class Spawn {
 
     public void tick() {
         if (hud.getWave() == 0) {
-            for (int i = 0; i < 5; i++) {
+            new Wave(handler, hud);
+            /*for (int i = 0; i < 5; i++) {
                 handler.addObject(new BasicEnemy(spawnX + 40 * i, spawnY, ID.BasicEnemy, handler, hud));
             }
-            hud.setWave(hud.getWave() + 1);
+            hud.setWave(hud.getWave() + 1);*/
         }
     }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
+    public HUD getHud() {
+        return hud;
+    }
+
+    public void setHud(HUD hud) {
+        this.hud = hud;
+    }
+
+    public int getSpawnX() {
+        return spawnX;
+    }
+
+    public void setSpawnX(int spawnX) {
+        this.spawnX = spawnX;
+    }
+
+    public int getSpawnY() {
+        return spawnY;
+    }
+
+    public void setSpawnY(int spawnY) {
+        this.spawnY = spawnY;
+    }
+    
 }
